b6096bc8c5577e3dae7c9d83729159ce811f67d2
HaciIsma <Haciismayilov5@outlook.com>
2020-05-23 05:39:45 +0400

    my FFFFFFFF

--- ViewModel/Window/MainViewModel.cs
+++ ViewModel/Window/MainViewModel.cs
@@ -1,16 +1,44 @@
 ﻿using PropertyChanged;
+using System.Windows.Input;
+using TradeProject.Command;
 
 namespace TradeProject.ViewModel.Window
 {
     [AddINotifyPropertyChangedInterface]
     public class MainViewModel
     {
-        public string PagePath { get; set; }
         public string LoginPagePath { get; set; }
+        public string PagePath { get; set; }
+        public ICommand AddCommand { get; set; }
+        public ICommand RemoveCommand { get; set; }
+        public ICommand HistoryCommand { get; set; }
+        public ICommand UpdateCommand { get; set; }
+
         public MainViewModel()
         {
+            //PagePath = "Pages/Store/StoreHome.xaml";
+            AddCommand = new RelayCommand(AddCommandExecute);
+            RemoveCommand = new RelayCommand(RemoveCommandExecute);
+            HistoryCommand = new RelayCommand(HistoryCommandExecute);
+            UpdateCommand = new RelayCommand(UpdateCommandExecute);
+
             LoginPagePath = "Pages/Login.xaml";
-            PagePath = "Pages/Store/StoreHome.xaml";
+            PagePath = "Pages/Store/StoreAdd.xaml";
+        }
+
+        private void AddCommandExecute(object param)
+        {
+            //PagePath = "Pages/Store/StoreAdd.xaml";
+        }
+        private void RemoveCommandExecute(object param)
+        {
+            // PagePath = "Pages/Store/StoreDelete.xaml";
+        }
+        private void HistoryCommandExecute(object param)
+        {
+        }
+        private void UpdateCommandExecute(object param)
+        {
         }
     }
 }
